303b6a77dbf4aace6366efefff617f9d882ee84f
Garrett Stonis <GStonis@emduggan.com>
2022-01-14 15:54:50 -0500

    Added basic functionality. And a very important icon.

---
+++ String Viewer/NotepadHelper.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
+
+
+namespace Notepad
+{
+    public static class NotepadHelper
+    {
+        [DllImport("user32.dll", EntryPoint = "SetWindowText")]
+        private static extern int SetWindowText(IntPtr hWnd, string text);
+
+        [DllImport("user32.dll", EntryPoint = "FindWindowEx")]
+        private static extern IntPtr FindWindowEx(IntPtr hwndParent, IntPtr hwndChildAfter, string lpszClass, string lpszWindow);
+
+        [DllImport("User32.dll", EntryPoint = "SendMessage")]
+        private static extern int SendMessage(IntPtr hWnd, int uMsg, int wParam, string lParam);
+
+
+        /// <summary>
+        /// Starts Notepad with the given <paramref name="text"/> and <paramref name="title"/>.
+        /// </summary>
+        /// <param name="text">The text to show in Notepad.</param>
+        /// <param name="title">The title to show on Notepad.</param>
+        /// <remarks>
+        /// Uses code found <see href="https://stackoverflow.com/questions/7613576/how-to-open-text-in-notepad-from-net/7613901#14295249">here</see>.
+        /// </remarks>
+        public static void ShowText(string text = null, string title = null)
+        {
+            Process notepad = Process.Start(new ProcessStartInfo("notepad.exe"));
+            if (notepad != null)
+            {
+                notepad.WaitForInputIdle();
+
+                if (!string.IsNullOrEmpty(title))
+                    SetWindowText(notepad.MainWindowHandle, title);
+
+                if (!string.IsNullOrEmpty(text))
+                {
+                    IntPtr child = FindWindowEx(notepad.MainWindowHandle, new IntPtr(0), "Edit", null);
+                    SendMessage(child, 0x000C, 0, text);
+                }
+            }
+        }
+    }
+}
--- String Viewer/Program.cs
+++ String Viewer/Program.cs
@@ -3,13 +3,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
+
 
 namespace String_Viewer
 {
-    class Program
+    public class Program
     {
+        [STAThread]
         static void Main(string[] args)
         {
+            if (Clipboard.ContainsText(TextDataFormat.Text))
+            {
+                string text = Clipboard.GetText(TextDataFormat.Text);
+
+                text = text.Replace("\\\'", "\'");
+                text = text.Replace("\\\"", "\"");
+                text = text.Replace("\\\\", "\\");
+                text = text.Replace("\\0", "\0");
+                text = text.Replace("\\a", "\a");
+                text = text.Replace("\\f", "\f");
+                text = text.Replace("\\n", "\n");
+                text = text.Replace("\\r", "\r");
+                text = text.Replace("\\t", "\t");
+                text = text.Replace("\\v", "\v");
+
+
+                Notepad.NotepadHelper.ShowText(text, "bruh");
+            }
         }
     }
 }
